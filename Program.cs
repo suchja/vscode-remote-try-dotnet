0cf7cc13b96c92e0aed82c48aba9a4d91db0c408
Jan (LernMoment.de) <jan@lernmoment.de>
2024-03-21 18:43:44 +0100

    Uses port 8080 instead of 5000

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Urls.Add("http://localhost:5000");
+app.Urls.Add("http://localhost:8080");
 
 app.MapGet("/", () => "Hello World!");
 
